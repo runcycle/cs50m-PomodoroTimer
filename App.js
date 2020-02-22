cc6b073161ae3eb89fa83f0ed68f04ff105aff40
runcycle <justinbartholomew@gmail.com>
2020-02-22 16:42:07 -0500

    Up and running

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hell Yes</Text>
     </View>
   );
 }
@@ -12,8 +12,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#7aadca',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });

71cd25fb2e6c019329be29215e5de6be514edf6e
runcycle <justinbartholomew@gmail.com>
2020-02-24 05:20:53 -0500

    Emulator test

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hell Yes</Text>
+      <Text>Smell Yes</Text>
     </View>
   );
 }
